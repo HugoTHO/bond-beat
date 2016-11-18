8402e5515fb779f955f3d9bbd2bf39809a9b4620
Hugo Oliveira <hugotholiveira@hotmail.com>
2016-11-18 14:51:36 -0300

    Desenhando os símbolos

---
+++ Assets/Scripts/BeatBondCS.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeatBondCS : MonoBehaviour {
+
+	public enum Stage { first, second, third, fourth };
+
+	public float beatTime, beatDistance = 1.42f;
+	public GameObject[] prefabs;
+	public int[] posCode;
+	public Transform spawnPointA, spawnPointB;
+
+	private int[] objBeats;
+	private float velocity, nextBeat;
+	private bool matched;
+	private Stage stage;
+	private GameObject[] objRefs;
+	private Vector3 posA, posB;
+	private InputFeedbackCS inputs;
+
+	// Use this for initialization
+	void Start () {
+
+		velocity = beatDistance / beatTime;
+		nextBeat = Time.time + beatTime;
+
+		posA = spawnPointA.transform.position;
+		posB = spawnPointB.transform.position;
+
+		stage = Stage.first;
+		matched = false;
+
+		objRefs = new GameObject[prefabs.Length];
+		objBeats = new int[prefabs.Length];
+
+		foreach (Transform child in transform.parent.parent)
+			if (child.CompareTag ("Input"))
+				inputs = child.GetComponent<InputFeedbackCS> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!matched) {
+
+			if (Time.time >= nextBeat) {
+
+				for (int i = (int)stage; i < objRefs.Length; i++) {
+				
+					if (objRefs [i] == null) {
+						if (i == 0) {
+							objRefs [i] = NewBeatLine (prefabs [i], posCode [i]);
+							objBeats [i] = 0;
+						} else if (objBeats [i - 1] == 2) {
+							objRefs [i] = NewBeatLine (prefabs [i], posCode [i]);
+							objBeats [i] = 0;
+						}
+					} else {
+					
+						objBeats [i] += 1;
+
+						if (objBeats [i] == 8) {
+							DestroyLine (objRefs [i], posCode [i]);
+							objRefs [i] = null;
+						}
+					}
+				}
+				nextBeat += beatTime;
+			}
+
+			if (objRefs [(int)stage] != null) {
+				float targetPosY = objRefs [(int)stage].transform.localPosition.y;
+
+				if (targetPosY <= -10.44f + (int)stage * 1.42f && stage != Stage.first) {
+					ResetState ();
+				} else {
+
+					if (inputs.InputCode > 0)
+					if (targetPosY >= -10.44f + (int)stage * 1.42f && targetPosY <= -9.44f + (int)stage * 1.42f &&
+					   inputs.InputCode == posCode [(int)stage]) {
+						NextStage ();
+					}
+
+				}
+			}
+		}
+	}
+
+	private void NextStage () {
+		
+		Rigidbody2D rbAux = objRefs[(int)stage].GetComponent<Rigidbody2D> ();
+		rbAux.velocity = Vector2.zero;
+		Vector3 auxVec = objRefs [(int)stage].transform.localPosition;
+		auxVec.y = -9.94f + (int)stage * 1.42f;
+		objRefs [(int)stage].transform.localPosition = auxVec;
+
+		inputs.NextStage ();
+
+		switch (stage) {
+			case Stage.first :
+				stage = Stage.second;
+				break;
+			case Stage.second :
+				stage = Stage.third;
+				break;
+			case Stage.third :
+				stage = Stage.fourth;
+				break;
+			case Stage.fourth:
+				Match ();
+				break;
+			}
+
+		if ((int)stage == objRefs.Length) {
+			Match ();
+		}
+	}
+
+	private void ResetState () {
+
+		for (int i = 0; i < (int)stage; i++) {
+			DestroyLine (objRefs [i], posCode [i]);
+		}
+
+		stage = Stage.first;
+		inputs.ResetState ();
+	}
+
+	private void Match () {
+		inputs.Block ();
+		matched = true;
+	}
+
+	private GameObject NewBeatLine (GameObject prefab, int posCode) {
+
+		GameObject aux;
+
+		switch (posCode) {
+		case 1: 
+			aux = (GameObject) Instantiate (prefab, posA, Quaternion.identity, this.transform);
+			break;
+		case 2:
+			aux = (GameObject) Instantiate (prefab, posB, Quaternion.identity, this.transform);
+			break;
+		case 3:
+			aux = new GameObject();
+			aux.AddComponent<Rigidbody2D> ();
+
+			Transform tAux = aux.transform;
+			tAux.SetParent(this.transform);
+			tAux.position = this.transform.position;
+
+			Instantiate (prefab, posA, Quaternion.identity, tAux);
+			Instantiate (prefab, posB, Quaternion.identity, tAux);
+			break;
+		default:
+			aux = new GameObject ();
+			aux.AddComponent<Rigidbody2D> ();
+			Debug.Log ("Comportamento Inesperado do Método 'NewBeatLine'");
+			break;
+		}
+			
+		Rigidbody2D rbAux = aux.GetComponent<Rigidbody2D> ();
+		rbAux.gravityScale = 0;
+		rbAux.velocity = Vector2.down * velocity;
+
+		return aux;
+	}
+
+	private void DestroyLine (GameObject reference, int posCode){
+
+		switch (posCode) {
+		case 1:
+		case 2:
+			GameObject.Destroy (reference);
+			break;
+		case 3:
+			GameObject.Destroy (reference.transform.GetChild (0).gameObject);
+			GameObject.Destroy (reference.transform.GetChild (1).gameObject);
+			GameObject.Destroy (reference);
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/InputFeedbackCS.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputFeedbackCS : MonoBehaviour {
+
+	public enum Stage { First, Second, Third, Fourth };
+
+	public KeyCode pressKey1, pressKey2;
+	public float pressDelay;
+	public int InputCode { get { return inputCode; } }
+
+	private int inputCode;
+	private bool delaying, blocked;
+	private float delayStart;
+	private Stage stage;
+	private GameObject[] childs;
+	private SpriteRenderer[] sprites;
+
+	void Start () {
+
+		childs = new GameObject[transform.childCount];
+		sprites = new SpriteRenderer[transform.childCount];
+
+		for (int i = 0; i < childs.Length; i++) {
+			childs [i] = this.transform.GetChild (i).gameObject;
+			sprites [i] = childs [i].GetComponent<SpriteRenderer> ();
+			childs [i].SetActive(false);
+		}
+
+		stage = Stage.First;
+		delaying = false;
+		blocked = false;
+		inputCode = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.W)) {
+			NextStage();
+		}
+
+		if (Input.GetKeyDown(KeyCode.R)) {
+			ResetState();
+		}
+
+		if (!blocked) {
+
+			if ((!delaying || Time.time - delayStart < 0.025) && Input.GetKeyDown(pressKey1)) {
+
+				childs[(int)stage * 2].SetActive (true);
+
+				if (!delaying) {
+					delayStart = Time.time;
+					delaying = true;
+				}
+			}
+
+			if ((!delaying || Time.time - delayStart < 0.025) && Input.GetKeyDown(pressKey2)) {
+
+				childs[(int)stage * 2 + 1].SetActive (true);
+
+				if (!delaying) {
+					delayStart = Time.time;
+					delaying = true;
+				}
+			}
+
+			if (delaying) {
+
+				float t = (pressDelay - (Time.time - delayStart)) / pressDelay;
+
+				if (childs [(int)stage * 2].activeSelf) {
+					Color tmp = sprites [(int)stage * 2].color;
+					tmp.a = t;
+					sprites [(int)stage * 2].color = tmp;
+				}
+				if (childs [(int)stage * 2 + 1].activeSelf) {
+					Color tmp = sprites [(int)stage * 2 + 1].color;
+					tmp.a = t;
+					sprites [(int)stage * 2 + 1].color = tmp;
+				}
+
+				if (Time.time - delayStart >= 0.025f) {
+
+					if (inputCode > 0)
+						inputCode = -1;
+
+					if (inputCode == 0) {
+						if (childs [(int)stage * 2].activeSelf && childs [(int)stage * 2 + 1].activeSelf)
+							inputCode = 3;
+						else if (childs [(int)stage * 2 + 1].activeSelf)
+							inputCode = 2;
+						else
+							inputCode = 1;
+					}
+				}
+
+				if (Time.time - delayStart >= pressDelay) {
+				
+					if (childs [(int)stage * 2].activeSelf) {
+						childs [(int)stage * 2].SetActive (false);
+					}
+					if (childs [(int)stage * 2 + 1].activeSelf) {
+						childs [(int)stage * 2 + 1].SetActive (false);
+					}
+
+					delaying = false;
+					inputCode = 0;
+				}
+
+			}
+		}
+	}
+
+	public void NextStage () {
+
+		delaying = false;
+		inputCode = 0;
+
+		if (childs [(int)stage * 2].activeSelf) {
+			Color tmp = sprites [(int)stage * 2].color;
+			tmp.a = 1f;
+			sprites [(int)stage * 2].color = tmp;
+		}
+		if (childs [(int)stage * 2 + 1].activeSelf) {
+			Color tmp = sprites [(int)stage * 2 + 1].color;
+			tmp.a = 1f;
+			sprites [(int)stage * 2 + 1].color = tmp;
+		}
+
+		switch (stage) {
+			case Stage.First :
+				stage = Stage.Second;
+				break;
+			case Stage.Second :
+				stage = Stage.Third;
+				break;
+			case Stage.Third :
+				stage = Stage.Fourth;
+				break;
+			case Stage.Fourth:
+				Block ();
+				break;
+		}
+	}
+
+	public void Block () {
+		blocked = true;
+	}
+
+	public void ResetState () {
+
+		for (int i = 0; i < childs.Length; i++)
+			childs [i].SetActive (false);
+
+		stage = Stage.First;
+		blocked = false;
+	}
+}
